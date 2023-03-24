9de1fe4df0727a42e65305924edab9a3f528e07f
MarekPuu <78322919+MarekPuu@users.noreply.github.com>
2023-03-24 12:54:52 +0200

    .

--- src/Pages/Talous.tsx
+++ src/Pages/Talous.tsx
@@ -4,11 +4,11 @@ import './styles/Talous.css';
 
 const Talous = () => {
   return (
-    <div className='talouspage_page_container'>
-      <div className='talouspage_left_container'>
+    <div className="talouspage_page_container">
+      <div className="talouspage_left_container">
         <DataTableContainer />
       </div>
-      <div className='talouspage_right_container'>
+      <div className="talouspage_right_container">
         {/* <DataTableContainer /> */}
       </div>
     </div>
