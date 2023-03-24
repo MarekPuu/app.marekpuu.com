6b476b08f1da5bc9ad27cad846d67100d2ef6280
MarekPuu <78322919+MarekPuu@users.noreply.github.com>
2023-03-24 12:52:24 +0200

    Routet hallitsemaan talouteen ohjausta

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,27 @@ import {
 import NotFoundPage from './Pages/NotFoundPage';
 import Talous from './Pages/Talous';
 import DefaultLayout from './components/Layout/DefaultLayout/DefaultLayout';
+import { useEffect } from 'react';
+import NavigateToTalousById from './Pages/NavigateToTalousById';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route element={<DefaultLayout />}>
-        <Route path='/' element={<Talous />} />
-        <Route path='/login' element={<NotFoundPage />} />
-        <Route path='*' element={<NotFoundPage />} />
+        <Route path="/" element={<NavigateToTalousById />} />
+        <Route path="/talous" element={<NavigateToTalousById />} />
+        <Route path="talous/:id" element={<Talous />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Route>
   )
 );
 
 function App() {
+  useEffect(() => {
+    document.body.setAttribute('data-theme', 'dark');
+  }, []);
+
   return <RouterProvider router={router} />;
 }
 
