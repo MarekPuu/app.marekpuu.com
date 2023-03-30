7819d97ea258553516509f7edd99ced04c6e8210
MarekPuu <78322919+MarekPuu@users.noreply.github.com>
2023-03-30 11:42:08 +0300

    .

--- src/components/HouseholdPage/MenotDataTable.tsx
+++ src/components/HouseholdPage/MenotDataTable.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useAxios } from '../../api/axios';
-import { getMenot } from '../../api/services/menot';
 import DataTable from '../DataTable/DataTable';
 
 const MenotDataTable = () => {
   const axios = useAxios();
-  const { isLoading, error, data } = useQuery('menotData', () =>
-    getMenot(axios, 1)
-  );
 
   return (
     <DataTable
@@ -19,8 +15,8 @@ const MenotDataTable = () => {
         { key: 'temperatureC', align: 'right', symbol: '€' },
       ]}
       title="Talouden menot"
-      tableRows={data?.data}
-      loading={isLoading}
+      tableRows={[]}
+      loading={true}
     />
   );
 };
